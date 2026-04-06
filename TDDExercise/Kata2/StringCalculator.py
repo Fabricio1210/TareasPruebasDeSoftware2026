4c6fe050741820cd8ef85b98f37dd84e796bdf49
Fabricio1210 <fabriciodlvalencia@gmail.com>
2026-04-06 12:39:31 -0600

    feat: kata 2 req 4 base code

--- TDDExercise/Kata2/StringCalculator.py
+++ TDDExercise/Kata2/StringCalculator.py
@@ -3,10 +3,14 @@ def add(cadena) -> int:
     splitArray = cadena.split(",", -1)
     #this method ensures any length is processed
     result = 0
+
+    if cadena.endswith(","):
+        raise ValueError("Weonazo terminala bien")
+
     for n in splitArray:
         if n == "":
             n = 0
         result += int(n)
     return result
 
-print(add("1,2,\n3"))
+print(add("1,2,\n3,"))
