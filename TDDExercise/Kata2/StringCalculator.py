0482c4c1b61ad6ec33718e6b99442b0ff02e45dc
Fabricio1210 <fabriciodlvalencia@gmail.com>
2026-04-06 12:32:45 -0600

    feat: kata 2 req 3 base code

--- TDDExercise/Kata2/StringCalculator.py
+++ TDDExercise/Kata2/StringCalculator.py
@@ -1,7 +1,12 @@
 def add(cadena) -> int:
+    cadena = cadena.replace("\n",",")
     splitArray = cadena.split(",", -1)
     #this method ensures any length is processed
     result = 0
     for n in splitArray:
+        if n == "":
+            n = 0
         result += int(n)
     return result
+
+print(add("1,2,\n3"))
--- TDDExercise/Kata2/TestStringCalculator.py
+++ TDDExercise/Kata2/TestStringCalculator.py
@@ -15,3 +15,10 @@ class TestStringCalculator(unittest.TestCase):
     
     def test_add_unknown_quantity_of_numbers(self):
         self.assertEqual(add("1,2,3,4,5,6,7,8,9,10"), 55)
+
+    def test_add_with_newline(self):
+        self.assertEqual(add("1,2\n3"), 6)
+    
+    def test_add_with_newline_error(self):
+        with self.assertRaises(Exception):
+            add("1,2,\n3")
