8e9e048d1945c191ae1d29eed830a184fb5f1e75
Fabricio1210 <fabriciodlvalencia@gmail.com>
2026-04-07 17:48:47 -0600

    feat: kata 3 req 5 test cases

--- TDDExercise/Kata3/TestPasswordValidation.py
+++ TDDExercise/Kata3/TestPasswordValidation.py
@@ -18,7 +18,7 @@ class TestPasswordValidation(unittest.TestCase):
 
     @patch('builtins.print')
     def test_password_valid_no_message(self, mock_print):
-        result = passwordValidation("Hola123456789")
+        result = passwordValidation("Hola123456789!")
         self.assertTrue(result)
         mock_print.assert_not_called()
 
@@ -28,12 +28,17 @@ class TestPasswordValidation(unittest.TestCase):
         mock_print.assert_called_once_with("password must contain at least one capital letter")
 
     @patch('builtins.print')
+    def test_missing_special_only(self, mock_print):
+        self.assertFalse(passwordValidation("Password12"))
+        mock_print.assert_called_once_with("password must contain at least one special character")
+
+    @patch('builtins.print')
     def test_multiple_errors(self, mock_print):
-        self.assertFalse(passwordValidation("abc1"))
-        
+        self.assertFalse(passwordValidation("abc1"))      
         mensaje_esperado = (
             "Password must be at least 8 characters\n"
             "The password must contain at least 2 numbers\n"
-            "password must contain at least one capital letter"
+            "password must contain at least one capital letter\n"
+            "password must contain at least one special character"
         )
         mock_print.assert_called_once_with(mensaje_esperado)
