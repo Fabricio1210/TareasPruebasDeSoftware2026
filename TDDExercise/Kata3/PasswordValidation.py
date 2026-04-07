76a8342986c04fc1b654b3658b0f025db117ee48
Fabricio1210 <fabriciodlvalencia@gmail.com>
2026-04-07 17:45:11 -0600

    feat: kata 3 req 4 base code

--- TDDExercise/Kata3/PasswordValidation.py
+++ TDDExercise/Kata3/PasswordValidation.py
@@ -1,17 +1,26 @@
 def passwordValidation(password) -> bool:
     errors = []
+
     if len(password) < 8:
         errors.append("Password must be at least 8 characters")
-
+        
     numeros_encontrados = 0
+    tiene_mayuscula = False
+    
     for caracter in password:
         if caracter.isdigit():
             numeros_encontrados += 1
-            
+        if caracter.isupper():
+            tiene_mayuscula = True
+
     if numeros_encontrados < 2:
         errors.append("The password must contain at least 2 numbers")
+    
+    if not tiene_mayuscula:
+        errors.append("password must contain at least one capital letter")
 
     if errors:
         print("\n".join(errors))
         return False
+
     return True
